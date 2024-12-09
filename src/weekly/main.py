cc9499b127195fe87877e989b5fdd1c8b64bc1a7
shadi-fsai <163780314+shadi-fsai@users.noreply.github.com>
2024-12-08 16:28:44 -0800

    update

--- src/weekly/main.py
+++ src/weekly/main.py
@@ -1,9 +1,14 @@
 from litellm import completion
 
-my_model = 'ollama/llama3.2'
-my_api_base = "http://localhost:11434"
+my_model = "groq/Llama-3.3-70b-Versatile" 
+my_api_base = None 
 
-def init():
+#my_model ='ollama/llama3.2'
+#my_api_base = "http://localhost:11434"
+
+
+def main():
+
     with open('orgcontext.txt', 'r') as file:
         org_context = file.read()
 
@@ -13,6 +18,7 @@ def init():
     with open('workstream-importance.txt', 'r') as file:
         workstream_importance = file.read()
 
+
     response = completion(
         model=my_model, 
         messages=[
@@ -20,17 +26,8 @@ def init():
             {"role": "system", "content": "Here is some context about your organization {" + org_context + "}"},
             {"role": "system", "content": "The following are examples of what highlights look like: {" + highlight_examples + "}"},
             {"role": "system", "content": "The following are the existing workstreams and their importance: {" + workstream_importance + "}"},            
-        ],
-        api_base=my_api_base  # Ollama's default address
-    )
 
-def main():
-    init()
-
-    response = completion(
-        model=my_model, 
-        messages=[
-            {"role": "user", "content": "Write a highlight for the following outcome: we hired a new head of operations to lead our new community launch. "}
+            {"role": "user", "content": "Write a highlight for the following outcome: we hired George Farah, a new head of operations to lead our new community launch."}
         ],
         api_base=my_api_base  # Ollama's default address
     )
